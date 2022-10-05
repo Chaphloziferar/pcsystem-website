f9db7774e14c0a604333a9a31479de8e492eebfd
Chaphloziferar <johnhectorlargaespada@gmail.com>
2022-10-05 00:38:16 -0600

    Created login screen

---
+++ src/views/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Home: React.FC = () => {
+  return (
+    <div className='flex'>
+      <div className='flex w-4/5 border-red-400 border-2'>
+        Home
+      </div>
+    </div>
+  )
+}
